e3ddefc97b4e748aee96d838b67f51e2fcc5e3e7
Unknown <ty13946@my.bristol.ac.uk>
2021-11-19 09:11:15 +0000

    Add SOAP vector selection

---
+++ mltrain/descriptors.py
@@ -0,0 +1,101 @@
+import numpy as np
+import mltrain as mlt
+from mltrain.log import logger
+from typing import Union, Optional, Sequence
+
+
+def soap_matrix(*args:    Union[mlt.ConfigurationSet, mlt.Configuration],
+                elements: Optional[Sequence] = None
+                ) -> np.ndarray:
+    """
+    Create a SOAP matrix using dscribe (https://github.com/SINGROUP/dscribe)
+    for a set of configurations
+
+    soap_matrix(config)           -> [[v0, v1, ..]]
+
+    soap_matrix(config1, config2) -> [[v0, v1, ..],
+                                      [u0, u1, ..]]
+
+    soap_matrix(configset)        -> [[v0, v1, ..],
+                                      ..]
+
+    ---------------------------------------------------------------------------
+    Arguments:
+        args: Configurations to use
+
+        elements: Atomic symbols for which the SOAP matrix should be made
+
+    Returns:
+         (np.ndarray): shape = (m, n) for m total configurations
+    """
+    # NOTE: import within function to allow mlt import without requirement
+    from dscribe.descriptors import SOAP
+
+    configurations = []
+
+    for item in args:
+        if isinstance(item, mlt.Configuration):
+            configurations.append(item)
+
+        elif isinstance(item, mlt.ConfigurationSet):
+            configurations.extend([c for c in item])
+
+        else:
+            raise ValueError(f'Could not calculate a SOAP vector for {item}')
+
+    logger.info(f'Calculating SOAP descriptor for {len(configurations)}'
+                f' configuration(s)')
+
+    if elements is None:
+        elements = list(set(atom.label for c in configurations
+                            for atom in c.atoms))
+
+    # Compute the average SOAP vector where the expansion coefficients are
+    # calculated over averages over each site
+    soap_desc = SOAP(species=elements,
+                     rcut=5,             # Distance cutoff (Å)
+                     nmax=6,             # Maximum order of the radial
+                     lmax=6,             # Maximum order of the angular
+                     average='inner')
+
+    soap_vec = soap_desc.create([conf.ase_atoms for conf in configurations])
+    logger.info('SOAP calculation done')
+
+    if soap_vec.ndim == 1:
+        # soap_desc.create doesn't return a consistent number of dimensions..
+        return soap_vec.reshape(1, -1)
+
+    return soap_vec
+
+
+def soap_kernel_vector(configuration:  mlt.Configuration,
+                       configurations: mlt.ConfigurationSet,
+                       zeta:           int = 4):
+    """
+    Calculate the kernel matrix between a set of configurations where the
+    kernel is:
+
+    .. math::
+
+        K(p_a, p_b) = (p_a . p_b / (p_a.p_a x p_b.p_b)^1/2 )^ζ
+
+    ---------------------------------------------------------------------------
+    Arguments:
+        configuration:
+
+        configurations:
+
+        zeta: Power to raise the kernel matrix to
+
+    Returns:
+        (np.ndarray): Vector, shape = len(configurations)
+    """
+
+    v1 = soap_matrix(configuration)[0]
+    v1 /= np.linalg.norm(v1)
+
+    # Normalised matrix over each soap vector (row)
+    m1 = soap_matrix(configurations)
+    m1 /= np.linalg.norm(m1, axis=1).reshape(len(configurations), 1)
+
+    return np.power(np.dot(m1, v1), zeta)
--- mltrain/training/selection.py
+++ mltrain/training/selection.py
@@ -1,6 +1,8 @@
+import numpy as np
 from abc import ABC, abstractmethod
 from typing import Optional
 from mltrain.log import logger
+from mltrain.descriptors import soap_kernel_vector
 
 
 class SelectionMethod(ABC):
@@ -91,3 +93,70 @@ class AbsDiffE(SelectionMethod):
     def too_large(self) -> bool:
         """|E_predicted - E_true| > 10*E_T"""
         return abs(self._configuration.energy.delta) > 10 * self.e_thresh
+
+
+class MaxAtomicEnvDistance(SelectionMethod):
+
+    def __init__(self,
+                 threshold: float = 0.98):
+        """
+        Selection criteria based on the maximum distance between any of the
+        training set and a new configuration. Evaluated based on the minimum
+        SOAP kernel vector (K*)
+
+        -----------------------------------------------------------------------
+        Arguments:
+            threshold:
+        """
+        super().__init__()
+
+        self.threshold = float(threshold)
+        self._k_vec = np.array([])
+
+    def __call__(self,
+                 configuration: 'mltrain.Configuration',
+                 mlp:           'mltrain.potentials.MLPotential',
+                 **kwargs) -> None:
+        """
+        Evaluate the selection criteria
+
+        -----------------------------------------------------------------------
+        Arguments:
+            configuration: Configuration to evaluate on
+
+            mlp: Machine learning potential with some associated training data
+        """
+        if len(mlp.training_data) == 0:
+            logger.warning('Have no training data - unable to determine '
+                           'criteria')
+            return
+
+        self._k_vec = soap_kernel_vector(configuration,
+                                         configurations=mlp.training_data)
+        return None
+
+    @property
+    def select(self) -> bool:
+        """
+        Determine if this configuration should be selected, based on the
+        minimum similarity between it and all of the training data
+
+        -----------------------------------------------------------------------
+        Returns:
+            (bool): If this configuration should be selected
+        """
+
+        if self._n_training_envs == 0:
+            return True
+
+        print(self._k_vec)
+        return np.min(self._k_vec) < self.threshold
+
+    @property
+    def too_large(self) -> bool:
+        return False
+
+    @property
+    def _n_training_envs(self) -> int:
+        """Number of training environments available"""
+        return len(self._k_vec)
