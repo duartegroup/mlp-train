6710c538d0dd7a7dbad74611cb6963a19f305419
tristan-j-wood <50619050+tristan-j-wood@users.noreply.github.com>
2022-06-28 09:18:54 +0100

    TS xyz files and py scripts for paper (#18)
    Required files to train a potential for each reaction in the paper

---
+++ examples/paper_examples/r1_fig2/mlp_comparison/gap/train.py
@@ -0,0 +1,30 @@
+import mltrain as mlt
+
+mlt.Config.n_cores = 10
+mlt.Config.orca_keywords = ['PBE0', 'def2-SVP', 'EnGrad']
+
+
+if __name__ == '__main__':
+
+    system = mlt.System(mlt.Molecule('ts_pbe0.xyz'),
+                        box=None)
+
+    gap = mlt.potentials.GAP('da',
+                             system=system)
+
+    gap.al_train(method_name='orca',
+                         temp=500,
+                         max_active_time=500,
+                         fix_init_config=True)
+
+
+    # Run some dynamics with the potential
+    trajectory = mlt.md.run_mlp_md(configuration=system.random_configuration(),
+                                   mlp=gap,
+                                   fs=500,
+                                   temp=300,
+                                   dt=0.5,
+                                   interval=10)
+
+    # and compare, plotting a parity diagram and E_true, ∆E and ∆F
+    trajectory.compare(gap, 'orca')
