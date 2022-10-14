266717ebf33ecaf75bd817991a09e6196ee7aeff
Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>
2022-10-14 16:52:46 +0100

    Update __init__.py

--- mlptrain/potentials/__init__.py
+++ mlptrain/potentials/__init__.py
@@ -1,7 +1,9 @@
 from mlptrain.potentials.gap.gap import GAP
 from mlptrain.potentials.ace.ace import ACE
+from mlptrain.potentials.mace.mace import MACE
 from mlptrain.potentials.nequip._nequip import NequIP
 
 __all__ = ['GAP',
            'ACE',
+           'MACE',
            'NequIP']
