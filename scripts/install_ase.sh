6f7c1984c11c1a8aa99d40f03d8eda89c25761d0
Valdas Vitartas <vitartc@gmail.com>
2023-02-09 16:07:33 +0000

    Update install_ase.sh

--- scripts/install_ase.sh
+++ scripts/install_ase.sh
@@ -1,7 +1,5 @@
 # Make sure you are in the /scripts directory before running this script
 echo "Installing ASE package from the master branch"
-conda install pip
-conda install git
 git clone https://github.com/rosswhitfield/ase.git
 pip install ./ase
 chmod -R +w ./ase && rm -r ./ase
