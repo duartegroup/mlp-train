d0334dde190b38f89b450bd358762e6b14273848
vitartas <vitartc@gmail.com>
2023-05-11 12:40:10 +0100

    Spelling

--- mlptrain/training/active.py
+++ mlptrain/training/active.py
@@ -202,7 +202,7 @@ def train(mlp:                 'mlptrain.potentials._base.MLPotential',
     if inherit_metad_bias:
         _remove_last_inherited_metad_bias_file(max_active_iters, bias)
 
-    logger.info(f'Final dataset size f = {mlp.n_train} Active learning = DONE')
+    logger.info(f'Final dataset size = {mlp.n_train} Active learning = DONE')
     return None
 
 
