f3f2db64973dfe474933e274ea422b469f9dd0aa
Valdas Vitartas <vitartc@gmail.com>
2023-06-16 15:07:21 +0100

    Modify MACE calculator

--- mlptrain/potentials/mace/mace.py
+++ mlptrain/potentials/mace/mace.py
@@ -6,6 +6,8 @@ import logging
 import numpy as np
 from typing import Optional, Dict, List
 from ase.data import chemical_symbols
+from ase.stress import full_3x3_to_voigt_6_stress
+from ase.calculators.calculator import Calculator, all_changes
 from mlptrain.potentials._base import MLPotential
 from mlptrain.config import Config
 from mlptrain.box import Box
@@ -19,9 +21,8 @@ try:
     from torch.optim.swa_utils import SWALR, AveragedModel
     from torch_ema import ExponentialMovingAverage
     from mace import data, modules, tools
-    from mace.tools import torch_geometric
+    from mace.tools import torch_geometric, torch_tools, utils
     from mace.tools.scripts_utils import create_error_table
-    from mace.calculators import MACECalculator
 except ModuleNotFoundError:
     pass
 
@@ -743,3 +744,83 @@ class MACE(MLPotential):
                 self._ema = None
 
         return self._ema
+
+
+class MACECalculator(Calculator):
+    """MACE ASE Calculator"""
+
+    implemented_properties = ["energy", "free_energy", "forces", "stress"]
+
+    def __init__(
+        self,
+        model_path: str,
+        device: str,
+        energy_units_to_eV: float = 1.0,
+        length_units_to_A: float = 1.0,
+        default_dtype="float64",
+        **kwargs
+    ):
+        Calculator.__init__(self, **kwargs)
+        self.results = {}
+
+        self.model = torch.load(f=model_path, map_location=device)
+        self.model = self.model.to(device)
+        self.r_max = float(self.model.r_max)
+        self.device = torch_tools.init_device(device)
+        self.energy_units_to_eV = energy_units_to_eV
+        self.length_units_to_A = length_units_to_A
+        self.z_table = utils.AtomicNumberTable(
+            [int(z) for z in self.model.atomic_numbers]
+        )
+
+        torch_tools.set_default_dtype(default_dtype)
+
+    # pylint: disable=dangerous-default-value
+    def calculate(self, atoms=None, properties=None, system_changes=all_changes):
+        """
+        Calculate properties.
+        :param atoms: ase.Atoms object
+        :param properties: [str], properties to be computed, used by ASE internally
+        :param system_changes: [str], system changes since last calculation, used by ASE internally
+        :return:
+        """
+        # call to base-class to set atoms attribute
+        Calculator.calculate(self, atoms)
+
+        # prepare data
+        config = data.config_from_atoms(atoms)
+        data_loader = torch_geometric.dataloader.DataLoader(
+            dataset=[
+                data.AtomicData.from_config(
+                    config, z_table=self.z_table, cutoff=self.r_max
+                )
+            ],
+            batch_size=1,
+            shuffle=False,
+            drop_last=False,
+        )
+        batch = next(iter(data_loader)).to(self.device)
+
+        # predict + extract data
+        out = self.model(batch.to_dict(), compute_stress=True)
+        energy = out["energy"].detach().cpu().item()
+        forces = out["forces"].detach().cpu().numpy()
+
+        # store results
+        E = energy * self.energy_units_to_eV
+        self.results = {
+            "energy": E,
+            "free_energy": E,
+            # force has units eng / len:
+            "forces": forces * (self.energy_units_to_eV / self.length_units_to_A),
+        }
+
+        # even though compute_stress is True, stress can be none if pbc is False
+        # not sure if correct ASE thing is to have no dict key, or dict key with value None
+        if out["stress"] is not None:
+            stress = out["stress"].detach().cpu().numpy()
+            # stress has units eng / len^3:
+            self.results["stress"] = (
+                stress * (self.energy_units_to_eV / self.length_units_to_A**3)
+            )[0]
+            self.results["stress"] = full_3x3_to_voigt_6_stress(self.results["stress"])
