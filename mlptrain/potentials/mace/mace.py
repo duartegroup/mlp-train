eb545348b5eea696725555cab8f2f7429b36d719
Valdas Vitartas <vitartc@gmail.com>
2023-06-19 15:33:56 +0100

    Update MACE calculator

--- mlptrain/potentials/mace/mace.py
+++ mlptrain/potentials/mace/mace.py
@@ -6,8 +6,7 @@ import logging
 import numpy as np
 from typing import Optional, Dict, List
 from ase.data import chemical_symbols
-from ase.stress import full_3x3_to_voigt_6_stress
-from ase.calculators.calculator import Calculator, all_changes
+from ase.calculators.calculator import Calculator
 from mlptrain.potentials._base import MLPotential
 from mlptrain.config import Config
 from mlptrain.box import Box
@@ -23,6 +22,7 @@ try:
     from mace import data, modules, tools
     from mace.tools import torch_geometric, torch_tools, utils
     from mace.tools.scripts_utils import create_error_table
+    from mace.calculators import MACECalculator as _MACECalculator
 except ModuleNotFoundError:
     pass
 
@@ -746,25 +746,24 @@ class MACE(MLPotential):
         return self._ema
 
 
-class MACECalculator(Calculator):
-    """MACE ASE Calculator"""
-
-    implemented_properties = ["energy", "free_energy", "forces", "stress"]
+class MACECalculator(_MACECalculator):
 
     def __init__(
-        self,
-        model_path: str,
-        device: str,
-        energy_units_to_eV: float = 1.0,
-        length_units_to_A: float = 1.0,
-        default_dtype="float64",
-        **kwargs
+            self,
+            model_path: str,
+            device: str,
+            energy_units_to_eV: float = 1.0,
+            length_units_to_A: float = 1.0,
+            default_dtype="float64",
+            **kwargs
     ):
         Calculator.__init__(self, **kwargs)
         self.results = {}
 
         self.model = torch.load(f=model_path, map_location=device)
-        self.model = self.model.to(device)
+        if device == 'cuda':
+            self.model = self.model.to(device)
+
         self.r_max = float(self.model.r_max)
         self.device = torch_tools.init_device(device)
         self.energy_units_to_eV = energy_units_to_eV
@@ -772,55 +771,4 @@ class MACECalculator(Calculator):
         self.z_table = utils.AtomicNumberTable(
             [int(z) for z in self.model.atomic_numbers]
         )
-
         torch_tools.set_default_dtype(default_dtype)
-
-    # pylint: disable=dangerous-default-value
-    def calculate(self, atoms=None, properties=None, system_changes=all_changes):
-        """
-        Calculate properties.
-        :param atoms: ase.Atoms object
-        :param properties: [str], properties to be computed, used by ASE internally
-        :param system_changes: [str], system changes since last calculation, used by ASE internally
-        :return:
-        """
-        # call to base-class to set atoms attribute
-        Calculator.calculate(self, atoms)
-
-        # prepare data
-        config = data.config_from_atoms(atoms)
-        data_loader = torch_geometric.dataloader.DataLoader(
-            dataset=[
-                data.AtomicData.from_config(
-                    config, z_table=self.z_table, cutoff=self.r_max
-                )
-            ],
-            batch_size=1,
-            shuffle=False,
-            drop_last=False,
-        )
-        batch = next(iter(data_loader)).to(self.device)
-
-        # predict + extract data
-        out = self.model(batch.to_dict(), compute_stress=True)
-        energy = out["energy"].detach().cpu().item()
-        forces = out["forces"].detach().cpu().numpy()
-
-        # store results
-        E = energy * self.energy_units_to_eV
-        self.results = {
-            "energy": E,
-            "free_energy": E,
-            # force has units eng / len:
-            "forces": forces * (self.energy_units_to_eV / self.length_units_to_A),
-        }
-
-        # even though compute_stress is True, stress can be none if pbc is False
-        # not sure if correct ASE thing is to have no dict key, or dict key with value None
-        if out["stress"] is not None:
-            stress = out["stress"].detach().cpu().numpy()
-            # stress has units eng / len^3:
-            self.results["stress"] = (
-                stress * (self.energy_units_to_eV / self.length_units_to_A**3)
-            )[0]
-            self.results["stress"] = full_3x3_to_voigt_6_stress(self.results["stress"])
