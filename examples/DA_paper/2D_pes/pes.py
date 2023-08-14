eb72020c2107db98b5a33908c486a191fa08d597
Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>
2023-08-14 10:46:30 +0100

    Update pes.py

--- examples/DA_paper/2D_pes/pes.py
+++ examples/DA_paper/2D_pes/pes.py
@@ -18,6 +18,7 @@ ade.Config.n_cores = 8
 
 ev_to_ha = 1.0 / 27.2114
 
+
 def adjust_potential_energy(self, atoms):
     """Returns the difference to the potential energy due to an active
     constraint. (That is, the quantity returned is to be added to the
@@ -40,7 +41,8 @@ def adjust_potential_energy(self, atoms):
     displace, _ = find_mic(p2 - p1, atoms.cell, atoms.pbc)
     bondlength = np.linalg.norm(displace)
     return 0.5 * self.spring * (bondlength - self.threshold)**2
-    
+
+
 def adjust_forces(self, atoms, forces):
     positions = atoms.positions
     if self._type == 'plane':
@@ -49,7 +51,8 @@ def adjust_forces(self, atoms, forces):
         d = ((A * x + B * y + C * z + D) /
          np.sqrt(A**2 + B**2 + C**2))
         if d < 0:
-            return
+            return None
+            
         magnitude = self.spring * d
         direction = - np.array((A, B, C)) / np.linalg.norm((A, B, C))
         forces[self.index] += direction * magnitude
@@ -57,23 +60,28 @@ def adjust_forces(self, atoms, forces):
         
     if self._type == 'two atoms':
         p1, p2 = positions[self.indices]
+        
     elif self._type == 'point':
         p1 = positions[self.index]
         p2 = self.origin
+        
     displace, _ = find_mic(p2 - p1, atoms.cell, atoms.pbc)
     bondlength = np.linalg.norm(displace)
     magnitude = self.spring * (bondlength - self.threshold)
     direction = displace / np.linalg.norm(displace)
+    
     if self._type == 'two atoms':
         forces[self.indices[0]] += direction * magnitude
         forces[self.indices[1]] -= direction * magnitude
+        
     else:
         forces[self.index] += direction * magnitude
+    return None
+
 
 def from_autode_to_ase (molecule, cell_size = 100):
-    # convert autode.molecule to ase.atoms 
-    # maintain the constrain generated during ade.pes.RelaxedPESnD calculation
-    
+    """ convert autode.molecule to ase.atoms 
+    maintain the constrain generated during ade.pes.RelaxedPESnD calculation"""    
     from ase.atoms import Atoms
     atoms = Atoms(symbols=[atom.label for atom in molecule.atoms],
                        positions=molecule.coordinates,
@@ -81,20 +89,15 @@ def from_autode_to_ase (molecule, cell_size = 100):
 
     atoms.set_cell([(cell_size, 0, 0), (0, cell_size, 0), (0, 0, cell_size)])
 
-    if molecule.constraints.distance is None:
-        pass
-
     c = []
-
     for (i, j), dist in molecule.constraints.distance.items():
         c.append(Hookean(a1 = i, a2 = j, k = 50, rt = dist))
-    atoms.set_constraint(c)
-  
+    atoms.set_constraint(c)  
     return atoms
 
 class MLPEST(ElectronicStructureMethod):
-    # class of machine learning potential fitted for autode package
-    # original code provided by T. Yang
+    """class of machine learning potential fitted for autode package
+    original code provided by T. Yang"""
 
     @property
     def available(self):
@@ -109,7 +112,7 @@ class MLPEST(ElectronicStructureMethod):
 
         molecule.print_xyz_file(filename=calc.input.filename)
         calc.input.additional_filenames = [self.path]
-        return
+        return None
 
     def get_output_filename(self, calc):
         return f'{calc.name}.xyz'
@@ -149,6 +152,7 @@ class MLPEST(ElectronicStructureMethod):
                                        n_cores=calc.n_cores)
                 name = self.get_output_filename(calc)
                 final_traj.save_xyz(filename=name, predicted = True)
+                
             else:
                 configuration = mlt.Configuration()
                 configuration.load(f'{calc.name}.xyz')
@@ -157,11 +161,13 @@ class MLPEST(ElectronicStructureMethod):
                                        n_cores=calc.n_cores)
                 name = self.get_output_filename(calc)
                 configuration.save_xyz(filename=name, predicted = True)
-
+                
         execute_mlp()
-        pass
+        return None
+        
     def calculation_terminated_normally(self, calc):
         name = self.get_output_filename(calc)
+        
         if os.path.exists(name):
             configuration = mlt.Configuration()
             configuration.load(name)
@@ -181,6 +187,7 @@ class MLPEST(ElectronicStructureMethod):
 
     def get_free_energy(self, calc):
         return None
+        
     def get_free_energy(self, calc):
         return None
 
@@ -215,7 +222,6 @@ class MLPEST(ElectronicStructureMethod):
         return configuration.forces.true * ev_to_ha
 
     def __init__(self, mlp, action, path):
-
         super().__init__(name='mlp', keywords_set=KeywordsSet(),
                          path='',
                          implicit_solvation_type=None)
@@ -224,32 +230,32 @@ class MLPEST(ElectronicStructureMethod):
         self.mlp = mlp
         self.action = deepcopy(action)      
 
+
 def get_final_species(TS, mlp):
-    # get the optimised product after MD propogation
+    """get the optimised product after MD propogation"""
     trajectory_product = mlt.md.run_mlp_md(configuration=TS,
                                        mlp=mlp,
                                        fs=500,
                                        temp=300,
                                        dt=0.5,
-                                       fbond_energy = {(1,12) : 0.1, (6,11) : 0.1},
+                                       fbond_energy={(1,12) : 0.1, (6,11) : 0.1},
                                        interval=2)
 
     final_traj_product = trajectory_product.final_frame
 
-    traj_product_optimised = optimise_with_fix_solute(solute = TS,
-                                                       configuration = final_traj_product,
-                                                       fmax = 0.01,
-                                                       mlp = mlp,
-                                                       constraint = False)
+    traj_product_optimised = optimise_with_fix_solute(solute=TS,
+                                                       configuration=final_traj_product,
+                                                       fmax=0.01,
+                                                       mlp=mlp,
+                                                       constraint=False)
 
     rt1 = np.linalg.norm(traj_product_optimised.atoms[1].coord-traj_product_optimised.atoms[12].coord)
     rt2 = np.linalg.norm(traj_product_optimised.atoms[6].coord-traj_product_optimised.atoms[11].coord)
-
     logger.info(f'the forming carbon bonds length in product are {rt1}, {rt2}')
 
     product = mlt.Molecule(name = 'product', atoms = traj_product_optimised.atoms)
-
     return  product
+
 
 @mlt.utils.work_in_tmp_dir(copied_exts=['.xml', '.json'])
 def optimise_with_fix_solute(solute, configuration, fmax, mlp, constraint = True, **kwargs):
@@ -259,7 +265,6 @@ def optimise_with_fix_solute(solute, configuration, fmax, mlp, constraint = True
     from ase.io.trajectory import Trajectory as ASETrajectory
 
     assert configuration.box != None, 'configuration must have box'
-
     logger.info('Optimise the configuration with fixed solute (solute coords should at the first in configuration coords) by MLP')
 
     n_cores = kwargs['n_cores'] if 'n_cores' in kwargs else min(Config.n_cores, 8)
@@ -284,7 +289,6 @@ def optimise_with_fix_solute(solute, configuration, fmax, mlp, constraint = True
 
     traj = _convert_ase_traj('tmp.traj')
     final_traj = traj.final_frame
-
     return final_traj
 
 Hookean.adjust_forces = adjust_forces
@@ -292,27 +296,27 @@ Hookean.adjust_potential_energy = adjust_potential_energy
 
 if __name__ == '__main__':
 
-    TS_mol = mlt.Molecule(name = 'cis_endo_TS_water.xyz')
+    TS_mol = mlt.Molecule(name='cis_endo_TS_water.xyz')
 
-    system = mlt.System(TS_mol, box = Box([100, 100, 100]))
+    system = mlt.System(TS_mol, box=Box([100, 100, 100]))
 
     endo = mlt.potentials.ACE('endo_ace_wB97M_imwater', system)
 
-    TS = mlt.Configuration(box = Box([100, 100, 100]))
-    TS.load(filename = 'cis_endo_TS_water.xyz', box = None)
+    TS = mlt.Configuration(box=Box([100, 100, 100]))
+    TS.load(filename='cis_endo_TS_water.xyz', box=None)
 
     cwd = os.getcwd()
-    ade_endo = MLPEST(mlp = endo, action = ['opt'], path = f'{cwd}/{endo.name}.json')
+    ade_endo = MLPEST(mlp=endo, action=['opt'], path=f'{cwd}/{endo.name}.json')
 
-    product = get_final_species(TS = TS, 
-                                mlp = endo)
+    product = get_final_species(TS=TS, 
+                                mlp=endo)
   
-    product.print_xyz_file(filename = 'product.xyz')
+    product.print_xyz_file(filename='product.xyz')
 
     pes = ade.pes.RelaxedPESnD(ade.Molecule('product.xyz'),
                                rs={(1, 12): (1.55, 3, 20),   # Current->3.0 Å in 8 steps
                                (6, 11): (1.55, 3, 20)})
 
     pes.calculate(method=ade_endo, keywords=['opt'], n_cores=8)
-    pes.save(filename = 'endo_in_water.npz')  
-    pes.plot(vmax = 65)
+    pes.save(filename='endo_in_water.npz')  
+    pes.plot()
