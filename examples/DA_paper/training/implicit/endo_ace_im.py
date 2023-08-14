5f7aee6941ddffb3b6b099c3124167b822919070
Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>
2023-08-14 11:04:37 +0100

    Update endo_ace_im.py

--- examples/DA_paper/training/implicit/endo_ace_im.py
+++ examples/DA_paper/training/implicit/endo_ace_im.py
@@ -7,7 +7,7 @@ mlt.Config.orca_keywords = ['wB97M-D3BJ', 'def2-TZVP','def2/J', 'RIJCOSX','EnGra
 if __name__ == '__main__':
     mol_TS = mlt.Molecule('cis_endo_TS_water.xyz')
 
-    TS = mlt.Configuration (charge = 0, mult =1 )
+    TS = mlt.Configuration (charge=0, mult=1)
     TS.atoms += mol_TS.atoms.copy()
 
     system = mlt.System(mol_TS,
@@ -18,9 +18,9 @@ if __name__ == '__main__':
 
     selector = MaxAtomicEnvDistance()
     ace.al_train(method_name='orca',
-                 selection_method = selector,
-                 max_active_time = 5000,
-                 fix_init_config = True,
+                 selection_method=selector,
+                 max_active_time=5000,
+                 fix_init_config=True,
                  temp=300)
 
     trajectory = mlt.md.run_mlp_md(configuration=TS,
