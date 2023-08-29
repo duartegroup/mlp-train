6b669cf94be037a1174c26c3ccab9d27f63c5b10
Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>
2023-08-29 10:19:43 +0100

    Update pes.py

--- examples/DA_paper/2D_pes/pes.py
+++ examples/DA_paper/2D_pes/pes.py
@@ -299,7 +299,7 @@ if __name__ == '__main__':
     endo = mlt.potentials.ACE('endo_ace_wB97M_imwater', system)
 
     TS = mlt.ConfigurationSet()
-    TS.load(filename='cis_endo_TS_water.xyz', box=Box([100, 100, 100]))
+    TS.load_xyz(filename='cis_endo_TS_water.xyz', box=Box([100, 100, 100]))
 
     cwd = os.getcwd()
     ade_endo = MLPEST(mlp=endo, action=['opt'], path=f'{cwd}/{endo.name}.json')
