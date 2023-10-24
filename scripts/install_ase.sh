14d0c26084c4b9af372fd402ef2aaaf05218df59
Daniel Hollas <danekhollas@gmail.com>
2023-10-25 00:58:43 +0100

    Install ASE from Gitlab (#63)
    The GitHub mirror of the ASE source code was removed, we switched to Gitlab.

--- scripts/install_ase.sh
+++
@@ -1,9 +0,0 @@
-# Make sure to be in the scripts directory and activate the environment
-# containing mlp-train and other packages required for the chosen machine
-# learning potential before running this script.
-
-# We need a newer ASE version to be able to use PLUMED interface
-# Here we use a specific commit from the main branch that is known to work.
-echo "* Installing ASE package from the master branch *"
-python -m pip install git+https://github.com/rosswhitfield/ase@f2615a6e9a
-echo "* DONE! *"
