f7e439a18d88cc20b609e58c0d52bfd81127ca96
Daniel Hollas <danekhollas@gmail.com>
2023-10-24 16:19:45 +0100

    Clean up install_mace.sh

--- scripts/install_mace.sh
+++ scripts/install_mace.sh
@@ -1,30 +1,30 @@
 # Make sure to be in the scripts directory before running this script
 
-echo "* Installing everything to an new conda environment called: mace *"
+set -euo pipefail
 
-# ----------------------------------------------------
-echo "* Installing mlp-train dependencies *"
-conda create --name mace python=3.9 --file ../requirements.txt -c conda-forge --yes
-conda activate mace
+PYTHON_VERSION="3.9"
 
-# ----------------------------------------------------
-echo "* Installing mlp-train *"
-pip install -e ../
+if [[ -n `which mamba` ]]; then
+  CONDAEXE=mamba
+elif [[ -n `which conda` ]]; then
+  CONDAEXE=conda
+else
+  echo "conda not found!"
+  exit 1
+fi
 
-# ----------------------------------------------------
-echo "* Installing PyTorch *"
-pip install torch torchvision torchaudio
+echo "* Installing everything to a new conda environment called: mace *"
 
-# ----------------------------------------------------
-echo "* Installing MACE dependencies *"
-pip install --upgrade e3nn
-pip install torch-ema
+echo "* Installing mlp-train dependencies via conda *"
+$CONDAEXE create --name mace python=${PYTHON_VERSION} --file ../requirements.txt -c conda-forge --yes
 
-# ----------------------------------------------------
-echo "* Installing MACE *"
-wget https://github.com/ACEsuit/mace/archive/refs/tags/v0.2.0.zip
-unzip v0.2.0.zip && rm v0.2.0.zip
-pip install mace-0.2.0/.
-rm -r mace-0.2.0
+# Install ASE from master branch
+./install_ase.sh
+
+echo "* Installing MACE and its dependencies (PyTorch, e3nn, ...) *"
+$CONDAEXE run -n mace pip install -r ../requirements_mace.txt
+
+echo "* Installing mlptrain in editable mode *"
+$CONDAEXE run -n mace pip install -e ../
 
 echo "* DONE! *"
