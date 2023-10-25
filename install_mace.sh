d946f7bba073f00e8ad6365e7cd950680fc75caf
Daniel Hollas <danekhollas@gmail.com>
2023-10-25 12:18:50 +0100

    Disable MACE again

--- install_mace.sh
+++ install_mace.sh
@@ -6,23 +6,20 @@ CONDA_ENV_NAME="mlptrain-mace"
 
 echo "* Looking for mamba or conda executable *"
 if which mamba; then
-  CONDAEXE=mamba
+    CONDA_EXE=mamba
 elif which micromamba; then
-    CONDAEXE=micromamba
+    CONDA_EXE=micromamba
 elif which conda; then
-  CONDAEXE=conda
+    CONDA_EXE=conda
 else
   echo "ERROR: conda executable not found!"
   exit 1
 fi
 
 echo "* Installing everything to a new conda environment called: ${CONDA_ENV_NAME} *"
-$CONDAEXE env create -n ${CONDA_ENV_NAME} --file environment_mace.yml
-
-#echo "* Installing MACE and its dependencies (PyTorch, e3nn, ...) *"
-#$CONDAEXE run -n ${CONDA_ENV_NAME} pip install -r requirements_mace.txt
+$CONDA_EXE env create -n ${CONDA_ENV_NAME} --file environment_mace.yml
 
 echo "* Installing mlptrain in editable mode *"
-$CONDAEXE run -n ${CONDA_ENV_NAME} pip install -e .
+$CONDA_EXE run -n ${CONDA_ENV_NAME} pip install -e .
 
 echo "* DONE! *"
