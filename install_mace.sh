f3ef35366c904a5ef8e122bf05780fb9a3439d7c
Veronika Juraskova <veronika.juraskova@chem.ox.ac.uk>
2023-10-30 17:57:18 +0000

    Fix openmmml install

--- install_mace.sh
+++ install_mace.sh
@@ -6,8 +6,4 @@ export CONDA_ENV_NAME=mlptrain-mace
 export CONDA_ENV_FILE=environment_mace.yml
 
 source create_conda_environment.sh
-echo "* Installing OpenMM-ML *"
-# NOTE: The upstream PR to openmm-ml has not been merged yet: https://github.com/openmm/openmm-ml/pull/61)
-pip install git+https://github.com/sef43/openmm-ml@mace
-
 echo "* DONE! *"
