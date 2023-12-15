b944f17d7556834671b00092ce9c469b2851192f
Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>
2023-12-15 13:45:15 +0000

    Update test_selection.py

--- mlptrain/training/tests/test_selection.py
+++ mlptrain/training/tests/test_selection.py
@@ -41,7 +41,7 @@ def test_selection_on_structures():
 
     assert len(configs) == 3
 
-    selector = MaxAtomicEnvDistance(threshold=0.9)
+    selector = AtomicEnvSimilarity (threshold=0.9)
     mlp = mlt.potentials.GAP('blank')
     mlp.training_data = configs
 
