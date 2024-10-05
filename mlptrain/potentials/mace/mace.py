4b09cb31227680abb78d59cd851e80ba40c24ae9
Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>
2024-10-05 10:59:05 +0100

    Update mace.py

--- mlptrain/potentials/mace/mace.py
+++ mlptrain/potentials/mace/mace.py
@@ -210,17 +210,14 @@ class MACE(MLPotential):
 
         logging.info('Generating error table')
 
-        all_collections = [
-            ('train', self.train_configs),
-            ('valid', self.valid_configs),
-        ]
+        all_collections = {
+            'training': self.train_loader, 
+            'validation': self.valid_loader,
+        }
 
         table = create_error_table(
             table_type=Config.mace_params['error_table'],
-            all_data_loaders={
-+                'training': self.train_loader,
-+                'validation': self.valid_loader,
-+            },
+            all_data_loaders=all_collections,
             model=self.model,
             loss_fn=self.loss_fn,
             output_args=self.output_args,
