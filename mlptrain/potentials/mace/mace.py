9c0b07efae3f78bd1e66b1e3a70ef3e964fd4e11
Martin Flerin <45570847+martinzola@users.noreply.github.com>
2024-10-09 16:16:34 +0100

    Fix issue with MACE error table following training. (#104)
    * Fix issue with MACE error table following training.
    
    * Update mace.py
    
    * Changes to error table function
    
    * Update mace.py
    
    * Update mace.py
    
    ---------
    
    Co-authored-by: Martin Ozbej Flerin Maver <wadh6506@aleph.chem.ox.ac.uk>
    Co-authored-by: Hanwen1018 <70756446+Hanwen1018@users.noreply.github.com>

--- mlptrain/potentials/mace/mace.py
+++ mlptrain/potentials/mace/mace.py
@@ -210,17 +210,14 @@ class MACE(MLPotential):
 
         logging.info('Generating error table')
 
-        all_collections = [
-            ('train', self.train_configs),
-            ('valid', self.valid_configs),
-        ]
+        all_collections = {
+            'training': self.train_loader, 
+            'validation': self.valid_loader,
+        }
 
         table = create_error_table(
             table_type=Config.mace_params['error_table'],
-            all_collections=all_collections,
-            z_table=self.z_table,
-            r_max=Config.mace_params['r_max'],
-            valid_batch_size=self.valid_batch_size,
+            all_data_loaders=all_collections,
             model=self.model,
             loss_fn=self.loss_fn,
             output_args=self.output_args,
