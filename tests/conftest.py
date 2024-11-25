6234495c80fa3c8b946c16f93e80594fc02651ee
Veronika Juraskova <veronika.juraskova@chem.ox.ac.uk>
2024-11-25 22:06:27 +0000

    Update water fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import mlptrain as mlt
 import pytest
+import numpy as np
 from autode.atoms import Atom
 
 
@@ -9,17 +10,6 @@ def h2():
     atoms = [
         Atom('H', -0.80952, 2.49855, 0.0),
         Atom('H', -0.34877, 1.961, 0.0),
-    ]
-    return mlt.Molecule(atoms=atoms, charge=0, mult=1)
-
-
-@pytest.fixture
-def h2o():
-    """Water molecule"""
-    atoms = [
-        Atom('H', 2.32670, 0.51322, 0.0),
-        Atom('H', 1.03337, 0.70894, -0.89333),
-        Atom('O', 1.35670, 0.51322, 0.0),
     ]
     return mlt.Molecule(atoms=atoms, charge=0, mult=1)
 
@@ -38,3 +28,18 @@ def h2o_configuration(h2o):
     config = system.random_configuration()
 
     return config
+
+
+@pytest.fixture
+def h2o():
+    """Create a water molecule (H2O) for testing."""
+    # Approximate coordinates for H2O with a 104.5° bond angle and 0.96 Å bond length
+    oxygen = Atom('O', 0.0, 0.0, 0.0)
+    hydrogen1 = Atom('H', 0.96, 0.0, 0.0)
+    hydrogen2 = Atom(
+        'H',
+        -0.96 * np.cos(np.radians(104.5)),
+        0.96 * np.sin(np.radians(104.5)),
+        0.0,
+    )
+    return mlt.Molecule(atoms=[oxygen, hydrogen1, hydrogen2], charge=0, mult=1)
