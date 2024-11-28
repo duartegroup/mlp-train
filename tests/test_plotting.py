f7a12281fd5367e062f7b3f8c8abcf3403667a26
JoeHolownia <JoeHolownia@github.com>
2024-11-28 11:18:42 +0000

    Modified plotting code to work with new list structure of true_energies and true_forces, finished test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -9,15 +9,18 @@ from mlptrain.utils import work_in_tmp_dir
 def test_parity_plot():
     config_set = ConfigurationSet(allow_duplicates=True)
 
-    for i in range(10):
+    for i in range(1000):
         config = Configuration(atoms=[Atom('H'), Atom('C'), Atom('C')])
+
+        # energies
         config.energy.true = 0.0 + (1.0 * i)
         config.energy.predicted = 1.0 + (1.0 * i)
-        config.forces.true = (1.0 + 1.0 * i) * np.ones(shape=(1, 3))
-        config.forces.predicted = (2.0 + 1.0 * i) * np.ones(shape=(1, 3))
-        config_set.append(config)
 
-    assert len(config_set) == 10
+        # forces
+        config.forces.true = np.random.uniform(1, 100, 3).reshape(1, 3)
+        config.forces.predicted = np.random.uniform(1, 100, 3).reshape(1, 3)
+
+        config_set.append(config)
 
     # simply check there are no issues running this code
     parity_plot(config_set)
