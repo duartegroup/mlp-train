aa642d565c1f5695fe69c3c57122c3f14887cfd2
zoe <zoe@zhangbinMBP13.local>
2025-02-12 17:05:56 +0000

    remove init_.py

--- mlptrain/descriptors/__init__.py
+++
@@ -1,5 +0,0 @@
-# from mlptrain.descriptors.ace_descriptor import ace_matrix, ace_kernel_vector
-# from mlptrain.descriptors.mace_descriptor import (mace_matrix,mace_kernel_vector,)
-# from mlptrain.descriptors.soap_descriptor import (soap_matrix,soap_kernel_vector,)
-
-# __all__ = ['ace_matrix','ace_kernel_vector','mace_matrix','mace_kernel_vector','soap_matrix','soap_kernel_vector',]
