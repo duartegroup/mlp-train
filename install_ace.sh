f63d4ca0f17656788a26a7ede34d8092314c01dd
Veronika Juraskova <veronika.juraskova@chem.ox.ac.uk>
2025-02-13 19:15:29 +0000

    Add JuLIP

--- install_ace.sh
+++ install_ace.sh
@@ -22,6 +22,7 @@ echo "using Pkg
 Pkg.Registry.add(\"General\")
 Pkg.Registry.add(RegistrySpec(url=\"https://github.com/ACEsuit/ACEregistry\"))
 Pkg.add(PackageSpec(name=\"ACEpotentials\",version=\"0.6.9\"))
+Pkg.add(PackageSpec(name=\"JuLIP\",version=\"0.14.2\"))
 Pkg.add(\"ExtXYZ\")
 Pkg.add(\"ASE\")" > add_julia_pkgs.jl
 
