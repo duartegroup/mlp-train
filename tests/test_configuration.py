ecacf86d5c049d603d0ab7b1037be8a84238a4df
Martin Flerin <45570847+martinzola@users.noreply.github.com>
2025-02-28 12:48:58 +0100

    Add solvation function (#137)
    Add solvation function as a method for Configuration, which allows the configuration to be solvated with a solvent of choice. The solvent can be selected from a database of solvents stored in autode with a corresponding density provided in a database in mlptrain. Alternatively, one can also specify their own solvent molecule and desired density. PR also includes tests for the function.

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,5 +1,13 @@
 from autode.atoms import Atom
-from mlptrain.configurations import Configuration
+from autode.exceptions import SolventNotFound
+from mlptrain.configurations.configuration import (
+    Configuration,
+    _random_vector_in_box,
+    _get_max_mol_distance,
+)
+import numpy as np
+import random
+import pytest
 
 
 def test_equality():
@@ -10,3 +18,74 @@ def test_equality():
     config2 = Configuration(atoms=[Atom('H')])
 
     assert config1 != config2
+
+
+seeded_random = random.Random()
+
+
+def test_random_vector_in_box():
+    vector = _random_vector_in_box(
+        10,
+        seeded_random.random(),
+        seeded_random.random(),
+        seeded_random.random(),
+    )
+    assert all(v <= 10 for v in vector)
+    assert all(v >= 0 for v in vector)
+
+
+def test_get_max_mol_distance(h2o_configuration):
+    max_distance_h2o = _get_max_mol_distance(h2o_configuration.atoms)
+    max_distance_h2o = round(max_distance_h2o, 3)
+    assert max_distance_h2o == 1.584
+
+
+def test_solvate(h2o_configuration, h2o_solvated_with_h2o):
+    h2o_configuration.solvate(solvent_name='water')
+    assert len(h2o_configuration.atoms) == 159
+    assert all(
+        [
+            np.round(atom.coordinate, 3)
+            == h2o_solvated_with_h2o.atoms[i].coordinate
+            for i, atom in enumerate(h2o_configuration.atoms)
+        ]
+    )
+
+
+def test_wrong_solvent_name_raises_not_found(h2o_configuration):
+    with pytest.raises(SolventNotFound):
+        h2o_configuration.solvate(solvent_name='solvo_solverson')
+
+
+def test_no_inputs_for_solvate(h2o_configuration):
+    with pytest.raises(ValueError):
+        h2o_configuration.solvate()
+
+
+def test_only_molecule_for_solvate(h2o_configuration, h2o):
+    with pytest.raises(ValueError):
+        h2o_configuration.solvate(solvent_molecule=h2o)
+
+
+def test_only_density_for_solvate(h2o_configuration):
+    with pytest.raises(ValueError):
+        h2o_configuration.solvate(solvent_density=1)
+
+
+def test_only_too_many_inputs_for_solvate(h2o_configuration, h2o):
+    with pytest.raises(ValueError):
+        h2o_configuration.solvate(
+            solvent_name='water', solvent_density=1, solvent_molecule=h2o
+        )
+
+
+def test_negative_density_for_solvate(h2o_configuration, h2o):
+    with pytest.raises(ValueError):
+        h2o_configuration.solvate(solvent_molecule=h2o, solvent_density=-1)
+
+
+def test_no_atoms_in_solvent_molecule(h2o_configuration, empty_molecule):
+    with pytest.raises(ValueError):
+        h2o_configuration.solvate(
+            solvent_density=1, solvent_molecule=empty_molecule
+        )
