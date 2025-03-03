1560f83be6985a23a57c0228e1b8c99f6abaa2e4
Zoe Zhang <zoezhang@fd09.chem.ox.ac.uk>
2025-03-03 17:53:12 +0000

    put doc strings back and delete xtb files

--- mlptrain/training/selection.py
+++ mlptrain/training/selection.py
@@ -46,6 +46,10 @@ class SelectionMethod(ABC):
         """
         Number of backtracking steps that this selection method should evaluate
         if the value is 'too_large'
+
+        -----------------------------------------------------------------------
+        Returns:
+            (int):
         """
 
     @property
@@ -62,10 +66,31 @@ class SelectionMethod(ABC):
 
 class AbsDiffE(SelectionMethod):
     def __init__(self, e_thresh: float = 0.1):
+        """
+        Selection method based on the absolute difference between the
+        true and predicted total energies.
+
+        -----------------------------------------------------------------------
+        Arguments:
+            e_thresh: E_T
+        """
         super().__init__()
+
         self.e_thresh = e_thresh
 
     def __call__(self, configuration, mlp, **kwargs) -> None:
+        """
+        Evaluate the true and predicted energies, used to determine if this
+        configuration should be selected.
+
+        -----------------------------------------------------------------------
+        Arguments:
+            configuration: Configuration that may or may not be selected
+
+            mlp: Machine learnt potential
+
+            method_name: Name of the reference method to use
+        """
         method_name = kwargs.get('method_name', None)
         self._configuration = configuration
 
@@ -85,6 +110,9 @@ class AbsDiffE(SelectionMethod):
 
     @property
     def select(self) -> bool:
+        """
+        10 E_T > |E_predicted - E_true| > E_T
+        """
         abs_dE = abs(self._configuration.energy.delta)
         logger.info(f'|E_MLP - E_true| = {abs_dE:.4} eV')
         return 10 * self.e_thresh > abs_dE > self.e_thresh
@@ -100,6 +128,16 @@ class AbsDiffE(SelectionMethod):
 
 class AtomicEnvSimilarity(SelectionMethod):
     def __init__(self, threshold: float = 0.999):
+        """
+        Selection criteria based on the maximum distance between any of the
+        training set and a new configuration. Evaluated based on the similarity
+        SOAP kernel vector (K*) between a new configuration and prior training
+        data
+
+        -----------------------------------------------------------------------
+        Arguments:
+            threshold: Value below which a configuration will be selected
+        """
         super().__init__()
 
         if threshold < 0.1 or threshold >= 1.0:
@@ -114,6 +152,15 @@ class AtomicEnvSimilarity(SelectionMethod):
         mlp: 'mlptrain.potentials.MLPotential',
         **kwargs,
     ) -> None:
+        """
+        Evaluate the selection criteria
+
+        -----------------------------------------------------------------------
+        Arguments:
+            configuration: Configuration to evaluate on
+
+            mlp: Machine learning potential with some associated training data
+        """
         if len(mlp.training_data) == 0:
             return None
 
@@ -125,6 +172,14 @@ class AtomicEnvSimilarity(SelectionMethod):
 
     @property
     def select(self) -> bool:
+        """
+        Determine if this configuration should be selected, based on the
+        minimum similarity between it and all of the training data
+
+        -----------------------------------------------------------------------
+        Returns:
+            (bool): If this configuration should be selected
+        """
         if self._n_training_envs == 0:
             return True
 
@@ -150,6 +205,28 @@ def outlier_identifier(
     distance_metric: str = 'euclidean',
     n_neighbors: int = 15,
 ) -> int:
+    """
+    This function identifies whether a new data (configuration)
+    is the outlier in comparison with the existing data (configurations) by Local Outlier
+    Factor (LOF). For more details about the LOF method, please see the lit.
+    Breunig, M. M., Kriegel, H.-P., Ng, R. T. & Sander, J. LOF: Identifying
+    density-based local outliers. SIGMOD Rec. 29, 93–104 (2000).
+
+    -----------------------------------------------------------------------
+    Arguments:
+
+    dim_reduction: if Ture, dimensionality reduction will
+                   be performed before LOF calculation (so far only PCA available).
+    distance_metric: distance metric used in LOF,
+                     which could be one of 'euclidean',
+                     'cosine' and 'manhattan’.
+    n_neighbors: number of neighbors considered when computing the LOF.
+
+    -----------------------------------------------------------------------
+    Returns:
+
+    -1 for anomalies/outliers and +1 for inliers.
+    """
     m1 = SoapDescriptor.compute_representation(configurations)
     m1 /= np.linalg.norm(m1, axis=1).reshape(len(configurations), 1)
 
@@ -167,6 +244,7 @@ def outlier_identifier(
         novelty=True,
         contamination=0.2,
     )
+    'contamination: define the porpotional of outliner in the data, the higher, the less abnormal'
     clf.fit(m1)
     new = clf.predict(v1)
 
@@ -180,6 +258,18 @@ class AtomicEnvDistance(SelectionMethod):
         distance_metric: str = 'euclidean',
         n_neighbors: int = 15,
     ):
+        """
+        Selection criteria based on analysis whether the configuration is
+        outlier by outlier_identifier function
+        -----------------------------------------------------------------------
+        Arguments:
+            pca: whether to do dimensionality reduction by PCA.
+                 As the selected distance_metric may potentially suffer from
+                 the curse of dimensionality, the dimensionality reduction step
+                 (using PCA) could be applied before calculating the LOF.
+                 This would ensure good performance in high-dimensional data space.
+            For the other arguments, please see details in the outlier_identifier function
+        """
         super().__init__()
         self.pca = pca
         self.metric = distance_metric
