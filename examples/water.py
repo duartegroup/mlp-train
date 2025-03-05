2a73d0231f519571afa447b1e2ea32f38e8bc68b
Veronika Juraskova <veronika.juraskova@email.cz>
2025-03-05 17:09:28 +0100

    Update example

--- examples/water.py
+++ examples/water.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     ace = mlt.potentials.ACE('water', system=system)
 
-    SoapDescriptor = SoapDescriptor(
+    descriptor = SoapDescriptor(
         average='outer', r_cut=6.0, n_max=6, l_max=6
     )
     selector = AtomicEnvSimilarity(descriptor=descriptor, threshold=0.9995)
