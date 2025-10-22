644e6f2b5e9c8ea3f0a0127d6fa2b1d90d15d123
Charlie Franklin <charlie.franklin@lincoln.ox.ac.uk>
2025-10-22 15:20:17 +0100

    Added optional arg to run_mlp_md (default False); only log output if True

--- mlptrain/sampling/md.py
+++ mlptrain/sampling/md.py
@@ -46,6 +46,7 @@ def run_mlp_md(
     restart_files: Optional[List[str]] = None,
     copied_substrings: Optional[Sequence[str]] = None,
     kept_substrings: Optional[Sequence[str]] = None,
+    verbose: bool = False,
     **kwargs,
 ) -> 'mlptrain.Trajectory':
     """
@@ -185,7 +186,9 @@ def run_mlp_md(
         **kwargs,
     )
 
-    logger.info('Finished MLP MD simulation')
+    if verbose:
+        logger.info('Finished MLP MD simulation')
+
     return traj
 
 
