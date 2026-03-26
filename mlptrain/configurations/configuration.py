807986d296040e210233b57604a1ee4323935d91
Veronika Juraskova <veronika.juraskova@chem.ox.ac.uk>
2026-03-26 13:23:41 +0000

    Initialisation of Configuration and ConfigurationSet from ORCA outputs (#181)
    Add class methods to initialise Configuration and ConfigurationSet from existing ORCA output files, including loading of true energies and forces. 
    
    ---------
    
    Co-authored-by: Josef Holownia <reub0549@htc-login01.arcus-htc.arc.local>
    Co-authored-by: joeho_ox_xps <reub0549@ox.ac.uk>
    Co-authored-by: JoeHolownia <joeholownia7@gmail.com>
    Co-authored-by: Daniel Hollas <danekhollas@gmail.com>
    Co-authored-by: Daniel Hollas <daniel.hollas@bristol.ac.uk>

--- mlptrain/configurations/configuration.py
+++ mlptrain/configurations/configuration.py
@@ -8,6 +8,7 @@ from typing import Optional, Union, List, Dict
 from copy import deepcopy
 from autode.atoms import AtomCollection, Atom
 import autode.atoms
+from autode.values import PotentialEnergy, Gradient
 import ase.atoms
 from mlptrain.log import logger
 from mlptrain.energy import Energy
@@ -90,6 +91,155 @@ class Configuration(AtomCollection):
         config = cls(atoms=atoms, charge=charge, mult=mult, box=box)
         if mol_dict:
             config.mol_dict = mol_dict
+
+        return config
+
+    @classmethod
+    def from_orca_file(
+        cls,
+        file_path: str,
+        *,
+        load_energy: bool = True,
+        load_forces: bool = True,
+    ) -> 'Configuration':
+        """
+        Return Configuration from existing ORCA calculation output files.
+
+        -----------------------------------------------------------
+        Arguments:
+
+        file_path: (str) Path to orca output file.
+
+        load_energy: (bool) If True, load energies from the files.
+
+        load_forces: (bool) If True, load forces from the files.
+        """
+
+        with open(file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        # TODO: Make sure we're actually looking at the ORCA output file!!!
+        # If not, it should be a hard error (but different from the one bellow).
+        # Look at how cclib does it.
+        if not any('ORCA TERMINATED NORMALLY' in line for line in lines):
+            raise RuntimeError(
+                f'ORCA did not terminate normally in file {file_path}.'
+            )
+
+        charge = None
+        mult = None
+        for cline in lines:
+            if 'Total Charge' in cline:
+                charge = int(cline.split()[4])
+
+        assert (
+            charge is not None
+        ), f'Could not determine charge from output file {file_path}'
+
+        for line in lines:
+            if 'Multiplicity' in line:
+                mult = int(line.split()[-1])
+
+        assert (
+            mult is not None
+        ), f'Could not determine multiplicity from output file {file_path}'
+        assert mult > 0, 'Multiplicity must be > 0'
+
+        read_coord = False
+        for line in lines:
+            if 'CARTESIAN COORDINATES (ANGSTROEM)' in line:
+                atoms = []
+                read_coord = True
+            elif line in ['\n', '\r\n']:
+                read_coord = False
+
+            if (
+                read_coord
+                and '----' not in line
+                and 'CARTESIAN COORDINATES (ANGSTROEM)' not in line
+            ):
+                element, x, y, z = line.split()
+                atom = ade.atoms.Atom(
+                    atomic_symbol=element,
+                    x=float(x),
+                    y=float(y),
+                    z=float(z),
+                )
+                atoms.append(atom)
+
+        num_atoms = len(atoms)
+
+        if load_energy:
+            if not any('FINAL SINGLE POINT ENERGY' in line for line in lines):
+                raise ValueError(
+                    f'Single point energy not found in output file {file_path}'
+                )
+
+            for en_line in reversed(lines):
+                if 'FINAL SINGLE POINT ENERGY' in en_line:
+                    energy = PotentialEnergy(en_line.split()[4], units='Ha')
+
+        if load_forces:
+            if not any(
+                ('CARTESIAN GRADIENT' in line)
+                or ('The final MP2 gradient' in line)
+                for line in lines
+            ):
+                raise ValueError(
+                    f'Gradients not found in output file {file_path}'
+                )
+
+            gradient_start = [
+                'CARTESIAN GRADIENT',
+                'The final MP2 gradient',
+            ]
+
+            gradient_ends = [
+                'Difference to translation invariance',
+                'Norm of the Cartesian gradient',
+                'NORM OF THE MP2 GRADIENT:',
+            ]
+
+            gradients = []
+            read_forces = False
+
+            for line in lines:
+                if any(substring in line for substring in gradient_start):
+                    read_forces = True
+                elif any(substring in line for substring in gradient_ends):
+                    read_forces = False
+
+                if read_forces:
+                    if len(line.split()) <= 3:
+                        continue
+                    else:
+                        dadx, dady, dadz = line.split()[-3:]
+
+                        gradients.append(
+                            [float(dadx), float(dady), float(dadz)]
+                        )
+
+            assert (
+                len(gradients) == num_atoms
+            ), f'Number of gradient lines ({len(gradients)}) != number of atoms ({num_atoms})'
+
+            forces = -Gradient(gradients, units='Ha a0^-1').to('Ha Å^-1')
+
+        # TODO: Add load_dipole parameter
+        # if load_dipoles:
+        #    for d_line in reversed(lines):
+        #        if 'Total Dipole Moment' in d_line:
+        #            dipx, dipy, dipz = line.split()[-3:]
+        #            dipole = [float(dipx), float(dipy), float(dipz)]
+
+        config = cls(atoms=atoms, charge=charge, mult=mult)
+
+        if load_energy:
+            config.energy.true = energy.to('eV')
+        if load_forces:
+            config.forces.true = forces.to('eV Å^-1')
+        # if load_dipole:
+        #   config.dipole.true = dipole.to()
 
         return config
 
