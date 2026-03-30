ca536e4dbf2a963be238375d609e93b4bc51b99b
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2026-03-30 23:41:05 +0100

    Shorten year to two digits

--- mlptrain/log.py
+++ mlptrain/log.py
@@ -1,9 +1,10 @@
 import logging
 import os
 
-_LOGGING_FORMAT = (
-    '%(asctime)s %(name)s[%(process)d] %(levelname)-8s %(message)s'
-)
+_LOGGING_FORMAT = '%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s'
+
+# Print year with only two digits
+_DATE_FORMAT = '%y-%m-%d %H:%M:%S'
 
 # Users can specify the logging level by setting the MLT_LOG_LEVEL environment variable before
 # running the python program. e.g. to only print warning message:
@@ -20,13 +21,15 @@ except AttributeError:
     print(f'Invalid value of MLT_LOG_LEVEL: "{ll}"')
     print('Falling back to INFO level')
 
-logging.basicConfig(level=_level, format=_LOGGING_FORMAT)
+logging.basicConfig(level=_level, format=_LOGGING_FORMAT, datefmt=_DATE_FORMAT)
 
 logger = logging.getLogger('mlptrain')
 # Try and use colourful logs
 try:
     import coloredlogs
 
-    coloredlogs.install(level=_level, logger=logger, fmt=_LOGGING_FORMAT)
+    coloredlogs.install(
+        level=_level, logger=logger, fmt=_LOGGING_FORMAT, datefmt=_DATE_FORMAT
+    )
 except ImportError:
     pass
